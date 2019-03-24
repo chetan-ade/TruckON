8f3c3ea36373864ffa3e0021675b6ff6a81a29e9
Chetan Ade <chetan.ade98@gmail.com>
2019-03-24 22:17:17 +0530

    added image carousel with sample text(no images)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,14 +8,42 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'TruckON',
+      theme: new ThemeData(         
+        //primaryColor: Colors.black,
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: Text('TruckON'),
         ),
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: OneTimeCarousel(),
       ),
+    );
+  }
+}
+
+class OneTimeCarousel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsetsDirectional.only(top: 30.0),
+      child: CarouselSlider(
+      height: 500.0,
+      enableInfiniteScroll: false,
+      items: [1, 2, 3].map((i) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.symmetric(horizontal: 5.0),
+                decoration: BoxDecoration(color: Colors.white),
+                child: Text(
+                  'text $i',
+                  style: TextStyle(fontSize: 16.0),
+                ));
+          },
+        );
+      }).toList(),
+    )
     );
   }
 }
