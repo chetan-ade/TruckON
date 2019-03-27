568fa170ca14ab6cc996e07e3085b801fb482383
Chetan Ade <chetan.ade98@gmail.com>
2019-03-27 18:35:37 +0530

    android files

--- lib/main.dart
+++ lib/main.dart
@@ -28,26 +28,29 @@ class OneTimeCarousel extends StatelessWidget {
       enableInfiniteScroll: false,
       items: ['assets/image1.jpg', 'assets/image2.jpg', 'assets/image3.jpg']
           .map((i) {
-            var heading;
-            if (i=='assets/image1.jpg') {
-              heading = 'SIMPLE SYSTEM';
-            } else if(i=='assets/image2.jpg') {
-              heading = 'MORE CUSTOMERS';
-            } else {
-              heading = 'EFFICIENT UTILIZATION';
-            }
+        var heading;
+        if (i == 'assets/image1.jpg') {
+          heading = 'SIMPLE SYSTEM';
+        } else if (i == 'assets/image2.jpg') {
+          heading = 'MORE CUSTOMERS';
+        } else {
+          heading = 'EFFICIENT UTILIZATION';
+        }
 
-            var desc;
-            if (i=='assets/image1.jpg') {
-              desc = 'Keep track of all appointments at any time and easily add new services or offers';
-            } else if(i=='assets/image2.jpg') {
-              desc = 'Gain more customers through the supraregional reach of a digital platform';
-            } else {
-              desc = 'Transport your load quickly and efficiently and thereby increase your turnover';
-            }
+        var desc;
+        if (i == 'assets/image1.jpg') {
+          desc =
+              'Keep track of all appointments at any time and easily add new services or offers';
+        } else if (i == 'assets/image2.jpg') {
+          desc =
+              'Gain more customers through the supraregional reach of a digital platform';
+        } else {
+          desc =
+              'Transport your load quickly and efficiently and thereby increase your turnover';
+        }
 
-            var height = MediaQuery.of(context).size.height;
-            var width = MediaQuery.of(context).size.width;
+        var height = MediaQuery.of(context).size.height;
+        var width = MediaQuery.of(context).size.width;
 
         return Builder(
           builder: (BuildContext context) {
@@ -57,26 +60,27 @@ class OneTimeCarousel extends StatelessWidget {
               decoration: BoxDecoration(
                   image: DecorationImage(
                       image: AssetImage(i), fit: BoxFit.fitHeight)),
-
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   Container(
-                      padding: EdgeInsets.only(top: height/10),
+                      padding: EdgeInsets.only(top: height / 10),
                       child: Text(
-                        'TruckON', 
-                        //Common Logo for all 3 images
+                        'TruckON',
                         textScaleFactor: 6.0,
                       )),
                   Container(
-                      padding: EdgeInsets.only(top: height*3/10),
+                      padding: EdgeInsets.only(top: height * 3 / 10),
                       child: Column(children: [
                         Text(
                           heading,
                           textScaleFactor: 2.5,
                         ),
                         Container(
-                            padding: EdgeInsets.only(top: height/100,left: width/40,right: width/40),
+                            padding: EdgeInsets.only(
+                                top: height / 100,
+                                left: width / 40,
+                                right: width / 40),
                             child: Text(
                               desc,
                               textScaleFactor: 1.5,
